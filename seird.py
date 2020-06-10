ad3e48e259fb4d2c67aee78bd7377bc3399116f2
Tom Buytaert <24655941+tbuytaer@users.noreply.github.com>
2020-06-10 22:39:16 +0200

    Move data loading code to a function

--- seird.py
+++ seird.py
@@ -5,81 +5,92 @@ import matplotlib.pyplot as plt
 # Clear the terminal
 os.system('clear')
 
-# Read data from Johns Hopkins
-file_jh_confirmed = './johns-hopkins-data/time_series_covid19_confirmed_global.csv'
-with open(file_jh_confirmed) as f:
-    reader = csv.reader(f)
-    header_row = next(reader)
-    hopkins_confirmed = []
-    for row in reader:
-        hopkins_confirmed.append(row)
+def load_data():
+    # Read data from Johns Hopkins
+    file_jh_confirmed = './johns-hopkins-data/time_series_covid19_confirmed_global.csv'
+    with open(file_jh_confirmed) as f:
+        reader = csv.reader(f)
+        header_row = next(reader)
+        hopkins_confirmed = []
+        for row in reader:
+            hopkins_confirmed.append(row)
 
-file_jh_deaths = './johns-hopkins-data/time_series_covid19_deaths_global.csv'
-with open(file_jh_deaths) as f:
-    reader = csv.reader(f)
-    header_row = next(reader)
-    hopkins_deaths = []
-    for row in reader:
-        hopkins_deaths.append(row)
+    file_jh_deaths = './johns-hopkins-data/time_series_covid19_deaths_global.csv'
+    with open(file_jh_deaths) as f:
+        reader = csv.reader(f)
+        header_row = next(reader)
+        hopkins_deaths = []
+        for row in reader:
+            hopkins_deaths.append(row)
 
-# List of ISO codes for countries
-file_countries = './ISO-countries.csv'
-with open(file_countries) as f:
-    reader = csv.reader(f)
-    header_row = next(reader)
-    countries_ISO = []
-    for row in reader:
-        countries_ISO.append(row)
+    # List of ISO codes for countries
+    file_countries = './ISO-countries.csv'
+    with open(file_countries) as f:
+        reader = csv.reader(f)
+        header_row = next(reader)
+        countries_ISO = []
+        for row in reader:
+            countries_ISO.append(row)
 
-# List of countries in Johns Hopkins files
-countries_ori = []
-for row in hopkins_confirmed:
-    countries_ori.append(row[1])
-countries_ori = sorted(set(countries_ori))
-
-# List of ISO countries that can be found in Johns Hopkins files. This is the one we will be using.
-countries = []
-for country_ori in countries_ori:
-    notfound = True
-    for country_ISO in countries_ISO:
-        if country_ori == country_ISO[1]:
-            notfound = False
-            countries.append(country_ISO)
-    
-# Number of days for which we have dat in the file
-countries_data = []
-number_of_days = len(hopkins_confirmed[1]) - 4
-print(number_of_days)
-for country in countries:
-    temp_confirmed = [0 for i in range(number_of_days)]
-    temp_deaths = [0 for i in range(number_of_days)]
-
-    # combine rows that belong to the same country
+    # List of countries in Johns Hopkins files
+    countries_ori = []
     for row in hopkins_confirmed:
-        if country[1] == row[1]:
-            #temp_confirmed = temp_confirmed + row[4:]
-            temp_confirmed = [int(temp_confirmed[i]) + int(row[4:][i]) for i in range(number_of_days)]
-    for row in hopkins_deaths:
-        if country[1] == row[1]:
-            temp_deaths = [int(temp_deaths[i]) + int(row[4:][i]) for i in range(number_of_days)]
+        countries_ori.append(row[1])
+    countries_ori = sorted(set(countries_ori))
 
-    # Create lists of differences
-    temp_confirmed_prev = temp_confirmed[:-1]
-    temp_confirmed_prev.insert(0,0)
-    temp_confirmed_delta = [int(temp_confirmed[i]) - int(temp_confirmed_prev[i]) for i in range(len(temp_confirmed))]
-    temp_deaths_prev = temp_deaths[:-1]
-    temp_deaths_prev.insert(0,0)
-    temp_deaths_delta = [int(temp_deaths[i]) - int(temp_deaths_prev[i]) for i in range(len(temp_deaths))]
-    
-    country_data = {
-        'name': country[1],
-        'confirmed': temp_confirmed,
-        'deaths': temp_deaths,
-        'confirmed_delta': temp_confirmed_delta,
-        'deaths_delta': temp_deaths_delta
-    }
-    countries_data.append(country_data)
+    # List of ISO countries that can be found in Johns Hopkins files. This is the one we will be using.
+    countries = []
+    for country_ori in countries_ori:
+        notfound = True
+        for country_ISO in countries_ISO:
+            if country_ori == country_ISO[1]:
+                notfound = False
+                countries.append(country_ISO)
+        if notfound:
+            print(f"Not found: {country_ori}")
 
+    # Number of days for which we have dat in the file
+    countries_data = []
+    number_of_days = len(hopkins_confirmed[1]) - 4
+    print(f"{number_of_days} days in data set")
+    for country in countries:
+        temp_confirmed = [0 for i in range(number_of_days)]
+        temp_deaths = [0 for i in range(number_of_days)]
+
+        # combine rows that belong to the same country
+        for row in hopkins_confirmed:
+            if country[1] == row[1]:
+                #temp_confirmed = temp_confirmed + row[4:]
+                temp_confirmed = [int(temp_confirmed[i]) + int(row[4:][i]) for i in range(number_of_days)]
+        for row in hopkins_deaths:
+            if country[1] == row[1]:
+                temp_deaths = [int(temp_deaths[i]) + int(row[4:][i]) for i in range(number_of_days)]
+
+        # Create lists of differences
+        temp_confirmed_prev = temp_confirmed[:-1]
+        temp_confirmed_prev.insert(0,0)
+        temp_confirmed_delta = [int(temp_confirmed[i]) - int(temp_confirmed_prev[i]) for i in range(len(temp_confirmed))]
+        temp_deaths_prev = temp_deaths[:-1]
+        temp_deaths_prev.insert(0,0)
+        temp_deaths_delta = [int(temp_deaths[i]) - int(temp_deaths_prev[i]) for i in range(len(temp_deaths))]
+        
+        country_data = {
+            'name': country[1],
+            'confirmed': temp_confirmed,
+            'deaths': temp_deaths,
+            'confirmed_delta': temp_confirmed_delta,
+            'deaths_delta': temp_deaths_delta
+        }
+        countries_data.append(country_data)
+    return countries_data
+
+
+
+countries_data = load_data()
+number_of_days = len(countries_data[1]['confirmed'])
+
+print(countries_data[16]['name'])
+print(countries_data[16]['confirmed'])
 
 # Plot a graph
 fig, ax = plt.subplots()
@@ -91,14 +102,3 @@ ax.scatter(x_values, countries_data[16]['confirmed_delta'],s=4)
 ax.scatter(x_values, countries_data[16]['deaths_delta'],s=4)
 plt.savefig("test.png")
 
-# Output to terminal
-print(f"JH Confirmed: {len(hopkins_confirmed)} rows")
-print(f"JH Deaths: {len(hopkins_deaths)} rows")
-print(f"{len(countries_ori)} countries ORI")
-print(f"{len(countries_ISO)} countries ISO")
-print(f"{len(countries)} countries NOW")
-print(temp_confirmed_delta)
-print(temp_confirmed)
-print(f"{len(countries_data)} countries data")
-print(countries_data[16]['name'])
-print(countries_data[16]['confirmed'])
