00aae0b10b24c3deac94bfb900f3b1a3ce14c213
Tom Buytaert <24655941+tbuytaer@users.noreply.github.com>
2020-06-13 00:21:05 +0200

    Start outputting JSON results

--- seird.py
+++ seird.py
@@ -3,6 +3,7 @@ import os
 import matplotlib.pyplot as plt
 import time
 import multiprocessing as mp
+import json
 
 from sirfunctions import SIR
 from sirfunctions import country_SIR
@@ -19,6 +20,7 @@ countries = JH_data['countries']
 
 number_of_days = len(countries_data[1]['confirmed'])
 
+future = 14
 
 start = time.time()
 print(f"\nStart: {time.asctime(time.localtime(start))}")
@@ -29,28 +31,67 @@ today = len(countries_data[1]['confirmed'])
 print("\nLast calculated Re per country:")
 
 def parallel_sir(country_id):
-    some_country = country_SIR(countries, countries_data, country_id)
+    some_country = country_SIR(countries, countries_data, country_id, average = 3, future = future)
     print(f"\t{countries_data[country_id]['name']} -> {some_country['r0'][-1]}")
-
-
+    country_sir = {
+        'country_id': country_id,
+        'name': countries_data[country_id]['name'],
+        'iso': countries[country_id][0],
+        'sir': some_country,
+    }
+    return country_sir
+    
 # Count the processors for parallel processing
 pool = mp.Pool(mp.cpu_count())
-pool.map(parallel_sir, [country_id for country_id in range(len(countries)) ] )
+countrysirs = pool.map(parallel_sir, [country_id for country_id in range(len(countries)) ] )
 
 pool.close()
-
-
 end = time.time()
 duration = end - start
 print(f"\nStart: {time.asctime(time.localtime(start))}")
 print(f"End: {time.asctime(time.localtime(end))}")
-print(f"Duration: {duration:.1f} seconds")
+#print(f"{countrysirs[16]}")
+
+
+print(f"{countrysirs[16]['country_id']} {countrysirs[16]['name']} {countrysirs[16]['sir']['cumulative']}")
+print(f"{countrysirs[16]['country_id']} {countrysirs[16]['name']} {countrysirs[16]['sir']['cumulative'][-(future + 1)]}")
+
+#jason = [{'id': countrysirs[country]['iso'], 'nr': countrysirs[country]['country_id'], 'value': countrysirs[country]['sir']['cumulative'][-(future + 1)] * 100_000 / int(countries[country][2])} for country in range(len(countrysirs))]
+# I want to output as float with 2 decimals, so f"{something:.2f}" to print it with 2 decimal places, and then convert back to a float
+
+# Cumulative cases
+jason = [{'id': countrysirs[country]['iso'], 'nr': countrysirs[country]['country_id'], 'value': float(f"{countrysirs[country]['sir']['cumulative'][-(future + 1)] * 100_000 / int(countries[country][2]):.2f}")} for country in range(len(countrysirs))]
+jason_file = 'export/world-cumulative.json'
+with open(jason_file, 'w') as f:
+    json.dump(jason, f, indent=4)
+
+# Active cases
+jason = [{'id': countrysirs[country]['iso'], 'nr': countrysirs[country]['country_id'], 'value': float(f"{countrysirs[country]['sir']['infected'][-(future + 1)] * 100_000 / int(countries[country][2]):.2f}")} for country in range(len(countrysirs))]
+jason_file = 'export/world-active.json'
+with open(jason_file, 'w') as f:
+    json.dump(jason, f, indent=4)
+
+
+# Deaths
+jason = [{'id': countrysirs[country]['iso'], 'nr': countrysirs[country]['country_id'], 'value': float(f"{countrysirs[country]['sir']['deaths'][-(future + 1)] * 100_000 / int(countries[country][2]):.1f}")} for country in range(len(countrysirs))]
+jason_file = 'export/world-deaths.json'
+with open(jason_file, 'w') as f:
+    json.dump(jason, f, indent=4)
+
+
+# Calculated Re
+jason = [{ 'id': countrysirs[country]['iso'], 'nr': countrysirs[country]['country_id'], 'value': countrysirs[country]['sir']['r0'][-(future + 1)] } for country in range(len(countrysirs))]
+jason_file = 'export/world-R0.json'
+with open(jason_file, 'w') as f:
+    json.dump(jason, f, indent=4)
+
+
 
 # Plot a graph
 fig, axs = plt.subplots(2, figsize=(15,10))
 plt.style.use('seaborn')
 chosen_country = 16
-some_country = country_SIR(countries, countries_data, chosen_country, average=2)
+some_country = country_SIR(countries, countries_data, chosen_country, average = 3, future = future)
 
 x_values = list(range(number_of_days))
 axs[0].scatter(x_values, countries_data[chosen_country]['confirmed'],s=4)
@@ -58,12 +99,12 @@ axs[0].scatter(x_values, countries_data[chosen_country]['deaths'],s=4)
 axs[0].scatter(x_values, countries_data[chosen_country]['d_confirmed'],s=4)
 axs[0].scatter(x_values, countries_data[chosen_country]['d_deaths'],s=4)
 
-x_values2 = list(range(number_of_days + 30))
+x_values2 = list(range(number_of_days + future))
 axs[0].plot(x_values2, some_country['cumulative'], linewidth=1)
 axs[0].plot(x_values2, some_country['deaths'], linewidth=1)
 axs[0].plot(x_values2, some_country['infected'], linewidth=1)
 
 axs[1].plot(x_values2, some_country['r0'], linewidth=1)
-axs[1].set_ylim([0,4])
+axs[1].set_ylim([0,5])
 
 plt.savefig('some-country.png', bbox_inches='tight')
